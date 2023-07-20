ee7de5cbde6a1bf5efc0b2960c0ce1b46a239ab1
Adin Ackerman <adinackerman@gmail.com>
2023-07-20 10:45:03 -0700

    Initial Commit

---
+++ ExamplePresentation/ExamplePresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplePresentationApp.swift
+//  ExamplePresentation
+//
+//  Created by Adin Ackerman on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamplePresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
